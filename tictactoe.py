7de5e8a419b04fd4ec1f98593c1faa0370df2c8f
Kaeli Nguyen <kanguyen21@bmchs.org>
2020-03-12 08:41:27 -0500

    this is my create task

---
+++ tictactoe.py
@@ -0,0 +1,25 @@
+#Clever Programmer (Youtube)
+
+board = ["_", "_", "_",
+         "_", "_", "_",
+         "_", "_", "_"]
+
+def display_board():
+    print(board[0], board[1], board[2])
+    print(board[3], board[4], board[5])
+    print(board[6], board[7], board[8])
+
+def play_game():
+    display_board()
+
+def turn(player):
+    position = input("Choose your move from 1-9: "}
+    position = int(position) - 1
+    
+
+play_game()
+
+        
+        
+    
+    
